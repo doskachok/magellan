01b7862c82753ea452c81eb0cff719b6e05a5cf7
Vyacheslav Kolesnik <vyacheslavk@automate.glass>
2022-07-02 19:16:40 +0300

    Add login page

---
+++ src/constants/localStorageKeys.ts
@@ -0,0 +1,4 @@
+export const LocalStorageKeys = {
+  TOKEN: 'ACCESS_TOKEN',
+  USER: 'USER'
+};
---
+++ src/features/auth/slice.ts
@@ -0,0 +1,48 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { LocalStorageKeys } from '../../constants/localStorageKeys';
+import { RootState } from '../../store';
+import { authApi } from './api';
+
+interface ISliceState {
+  user: { email: string } | null;
+  accessToken: string | null;
+}
+
+interface IAuthPayload {
+  email: string,
+  token: string
+}
+
+const initialState: ISliceState = {
+  accessToken: localStorage.getItem(LocalStorageKeys.TOKEN),
+  user: JSON.parse(<string>localStorage.getItem(LocalStorageKeys.USER))
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: initialState,
+  reducers: {
+    logOut: (state) => {
+      state.accessToken = null;
+      state.user = null;
+      localStorage.clear();
+    }
+  },
+  extraReducers: builder => {
+    builder.addMatcher(
+      authApi.endpoints.login.matchFulfilled,
+      (state, { payload }: { payload: IAuthPayload }) => {
+        localStorage.setItem(LocalStorageKeys.TOKEN, payload.token);
+        localStorage.setItem(LocalStorageKeys.USER, JSON.stringify({ email: payload.email }));
+
+        state.accessToken = payload.token;
+        state.user = { email: payload.email };
+      }
+    )
+  }
+});
+
+export const { logOut } = authSlice.actions;
+export default authSlice.reducer;
+
+export const userSelector = (store: RootState) => store.auth.user;
