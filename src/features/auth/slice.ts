8a2955d92cdfa3340bb16c2d6648a2000eb8e729
Denys Krutko <krutko.den@gmail.com>
2022-08-17 11:37:56 +0200

    - refresh token (doesnt work)

--- src/features/auth/slice.ts
+++ src/features/auth/slice.ts
@@ -1,16 +1,18 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { LocalStorageKeys } from '../../constants/localStorageKeys';
 import { RootState } from '../../store';
-import { authApi } from './api';
+// import { authApi } from './api';
 import { IAuthPayload, IUser } from './types';
 
 interface ISliceState {
   user: IUser | null;
   accessToken: string | null;
+  userId: string | null;
 }
 
 const initialState: ISliceState = {
   accessToken: localStorage.getItem(LocalStorageKeys.TOKEN),
+  userId: localStorage.getItem(LocalStorageKeys.),
   user: JSON.parse(localStorage.getItem(LocalStorageKeys.USER) as string) as IUser,
 };
 
@@ -22,9 +24,17 @@ const authSlice = createSlice({
       state.accessToken = null;
       state.user = null;
       localStorage.clear();
+    },
+    tokenReceived: (state, { payload }: PayloadAction<any>) => {
+      state.accessToken = payload.token;
+      localStorage.setItem(LocalStorageKeys.TOKEN, payload.token);
+    },
+    userReceived: (state, { payload }: PayloadAction<IUser>) => {
+      localStorage.setItem(LocalStorageKeys.USER, JSON.stringify(payload));
+      state.user = payload;
     }
   },
-  extraReducers: builder => {
+  /* extraReducers: builder => {
     builder
     .addMatcher(
       authApi.endpoints.login.matchFulfilled,
@@ -40,10 +50,10 @@ const authSlice = createSlice({
         state.user = payload;
       }
     )
-  }
+  } */
 });
 
-export const { logOut } = authSlice.actions;
+export const { tokenReceived, logOut, userReceived } = authSlice.actions;
 export default authSlice.reducer;
 
 export const userSelector = (store: RootState) => store.auth.user;
--- src/store/api.ts
+++ src/store/api.ts
@@ -1,28 +1,63 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/dist/query/react';
+import { BaseQueryApi } from '@reduxjs/toolkit/dist/query/baseQueryTypes';
+import {
+  createApi, 
+  BaseQueryFn,
+  FetchArgs,
+  fetchBaseQuery,
+  FetchBaseQueryError
+} from '@reduxjs/toolkit/dist/query/react';
 import { ApiTags } from '../constants/api-tags';
 import { RootState } from './index';
+import { addError } from './errorSlice';
+import { tokenReceived, logOut, userReceived } from '../features/auth/slice'
+import { IAuthPayload } from '../features/auth/types';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
+
   prepareHeaders: (headers, {getState}) => {
     const token = (getState() as RootState).auth.accessToken;
 
     if (token) {
       headers.set('Authorization', `Bearer ${token}`);
     }
-
     return headers;
   }
 });
 
+const baseQueryWithReAuth: BaseQueryFn<
+  string | FetchArgs, 
+  unknown, 
+  FetchBaseQueryError> 
+  = async (args: string | FetchArgs, api: BaseQueryApi, extraOptions: {}
+) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  console.log('args', args);
+  console.log('api', api);
+
+  if (result.error && result.error.status === 401) {
+      const refreshed = await baseQuery({ url: 'users/refreshed-token'}, api, extraOptions);
+
+      if (refreshed.data) {
+        api.dispatch(tokenReceived(refreshed.data));
+        result = await baseQuery(args, api, extraOptions);
+      } else {
+        api.dispatch(logOut());
+        api.dispatch(addError({ message: 'Please log in again'}));
+      }
+  }
+
+  return result;
+};
+
 export const mainApi = createApi({
   reducerPath: 'mainApi',
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithReAuth,
   tagTypes: Object.values(ApiTags.GROUPS_LIST),
-  endpoints: (build) => ({
-  })
+  endpoints: () => ({ })
 });
 
 export default mainApi;
