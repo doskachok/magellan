2366bf7215d8ee95181ab6f2c8146e997e2a072c
Viktor Iskra <viktor.iskra@adup.com.ua>
2023-01-12 23:29:29 +0200

    Cache store data using localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes, Navigate, Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
 import { Provider, useSelector } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { ThemeProvider } from 'styled-components';
 import { Toaster } from 'react-hot-toast';
 
 import theme from './constants/theme/default.theme';
 
-import store from './store';
+import store, { persistor } from './store';
 
 import { ROUTES } from './constants/routes';
 
@@ -22,31 +24,37 @@ const PrivateRoutes = () => {
   return token ? <Outlet /> : <Navigate to={ROUTES.AUTH.ROOT} />;
 };
 
+const Loader = () => (<p>Loading...</p>);
+
 function App() {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <ModalProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path={ROUTES.ROOT} element={<PrivateRoutes />}>
-                <Route index element={<Navigate to={ROUTES.GROUPS.ROOT} />} />
-                <Route path={`${ROUTES.GROUPS.ROOT}/*`} element={<Groups />} />
-                <Route path={`${ROUTES.ACCOUNT_SETTINGS}/*`} element={<AccountSettings />} />
+    <Suspense fallback={<Loader/>}>
+      <Provider store={store}>
+        <PersistGate persistor={persistor} loading={null}>
+          <ThemeProvider theme={theme}>
+            <ModalProvider>
+              <BrowserRouter>
+                <Routes>
+                  <Route path={ROUTES.ROOT} element={<PrivateRoutes />}>
+                    <Route index element={<Navigate to={ROUTES.GROUPS.ROOT} />} />
+                    <Route path={`${ROUTES.GROUPS.ROOT}/*`} element={<Groups />} />
+                  <Route path={`${ROUTES.ACCOUNT_SETTINGS}/*`} element={<AccountSettings />} />
               </Route>
-              <Route path={`${ROUTES.AUTH.ROOT}/*`} element={<Auth />} />
-            </Routes>
-            <ErrorNotifier />
-          </BrowserRouter>
-          <Toaster
-            position='bottom-center'
-            toastOptions={{
-              duration: 3000,
-            }}
-          />
-        </ModalProvider>
-      </ThemeProvider>
-    </Provider>
+                  <Route path={`${ROUTES.AUTH.ROOT}/*`} element={<Auth />} />
+                </Routes>
+                <ErrorNotifier />
+              </BrowserRouter>
+              <Toaster
+                position='bottom-center'
+                toastOptions={{
+                  duration: 3000,
+                }}
+              />
+            </ModalProvider>
+          </ThemeProvider>
+        </PersistGate>
+      </Provider>
+    </Suspense>
   );
 }
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { 
+  persistStore, 
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import {Middleware} from 'redux';
 import {createLogger} from 'redux-logger'
 import {mainApi} from './api';
@@ -44,15 +55,33 @@ const middlewares: Middleware[] = [
   logger,
 ];
 
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    groups: groupsReducer,
-    errors: errorReducer,
-    ...apiReducers,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middlewares)
+const reducer = combineReducers({
+  auth: authReducer,
+  groups: groupsReducer,
+  errors: errorReducer,
+  ...apiReducers,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['groups'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
+      .concat(middlewares)
+});
+
+export const persistor = persistStore(store);
 
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
