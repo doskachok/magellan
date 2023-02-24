34971fd6d430917dfb77e9f769e0878c1795e070
Vasyl Horbatenko <horbatenko.vasyl@gmail.com>
2023-02-24 21:46:04 +0200

    Add basic page header.

--- src/features/groups/Details/index.tsx
+++ src/features/groups/Details/index.tsx
@@ -1,6 +1,30 @@
+import { useCallback } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { PageWrapper } from 'components/Containers';
+import Header from 'components/Header';
+import { ROUTES } from 'constants/routes';
+import { useGetTransactionGroupByIdQuery } from '../api';
+import { ReactComponent as BackIconSVG } from 'assets/images/back-icon.svg';
+
 const GroupDetails = () => {
+  const navigate = useNavigate();
+  const { groupId } = useParams();
+
+  const { data: group } = useGetTransactionGroupByIdQuery(groupId || '');
+
+  const handleBackAction = useCallback(() => {
+    navigate(ROUTES.GROUPS.ROOT, { replace: true });
+  }, [navigate]);
+
   return (
-    <></>
+    <PageWrapper>
+      <Header
+        text={group ? group.name : ''}
+        leftActionComponent={<BackIconSVG onClick={handleBackAction} />}
+      />
+
+    </PageWrapper>
   );
 };
 
