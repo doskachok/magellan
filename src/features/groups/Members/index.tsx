4bc0070f9ec5ce18bcb26057976ae31e1ba199f6
Vasyl Horbatenko <horbatenko.vasyl@gmail.com>
2023-03-11 19:30:49 +0200

    Update user list on the member screen after adding/removing member.

--- src/features/groups/Members/index.tsx
+++ src/features/groups/Members/index.tsx
@@ -1,51 +1,58 @@
-import { memo, useCallback, useState } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { Column, Row } from 'components/Containers';
 import { ContentWrapper, RemoveBtn } from './index.styled';
 import { TextRegular, Button } from 'components';
-import {ITransactionGroup} from '../types';
 import { useModal } from 'providers/ModalProvider';
 import AddMemberModal from './AddMemberModal';
 import { IUser } from 'types/userTypes';
-import { useAddParticipantMutation, useRemoveParticipantMutation } from '../api';
+import { useAddParticipantMutation, useLazyGetTransactionGroupByIdQuery, useRemoveParticipantMutation } from '../api';
 import Loader from 'components/Loader';
 import MemberRow from './MemberRow';
 
 export interface IGroupMembersProps {
-  group: ITransactionGroup | undefined;
+  groupId: string;
 }
 
-const GroupMembers = ({ group }: IGroupMembersProps) => {
+const GroupMembers = ({ groupId }: IGroupMembersProps) => {
   const { t } = useTranslation('groups');
   const modalContext = useModal();
 
+  const [getGroup, {data: group, isLoading: isGroupLoading }] = useLazyGetTransactionGroupByIdQuery();
   const [addMember, { isLoading: isMemberAdding }] = useAddParticipantMutation();
   const [removeMember, { isLoading: isMemberRemoving }] = useRemoveParticipantMutation();
   const [selected, setSelected] = useState<IUser | null>(null);
 
   const handleMemberToAddSelected = useCallback((member: IUser) => {
-    addMember({ groupId: group?.id || '', userId: member.id });
-  }, [addMember, group]);
+    addMember({ groupId, userId: member.id }).then(() => {
+      getGroup(groupId);
+    });
+  }, [addMember, groupId, getGroup]);
 
   const onAddMember = useCallback(() => {
     modalContext.showModal(<AddMemberModal onMemberSelected={handleMemberToAddSelected} />);
   }, [modalContext, handleMemberToAddSelected]);
 
   const onRemoveMember = useCallback(() => {
-    removeMember({ groupId: group?.id || '', userId: selected!.id }).then(() => {
+    removeMember({ groupId, userId: selected!.id }).then(() => {
       setSelected(null);
+      getGroup(groupId);
     });
-  }, [group, selected, removeMember]);
+  }, [groupId, selected, removeMember, getGroup]);
 
   const handleMemberSelected = useCallback((member: IUser) => {
     setSelected(member);
   }, [setSelected]);
 
+  useEffect(() => {
+    getGroup(groupId);
+  }, [groupId, getGroup]);
+
   return (
     <ContentWrapper jc={'space-between'} fullWidth>
       <Column fullWidth>
-        {group?.participants?.map(p =>
+        {group?.participants?.map((p: IUser) =>
           <MemberRow key={p.id} isSelected={selected === p} member={p} onClick={handleMemberSelected} />
         )}
 
@@ -60,7 +67,7 @@ const GroupMembers = ({ group }: IGroupMembersProps) => {
         }
       </Column>
 
-      <Loader isLoading={isMemberAdding || isMemberRemoving} />
+      <Loader isLoading={isMemberAdding || isMemberRemoving || isGroupLoading} />
 
       <Row jc={selected ? 'space-between' : 'flex-end'} fullWidth>
         {
