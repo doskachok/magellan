e49956e8a42c2b5e5ff8d988baed40bb988390a2
Vasyl Horbatenko <horbatenko.vasyl@gmail.com>
2023-03-11 16:42:25 +0200

    Add ability to select already known users in AddMemberModal.

--- src/features/groups/Members/AddMemberModal/index.tsx
+++ src/features/groups/Members/AddMemberModal/index.tsx
@@ -1,10 +1,15 @@
 import { Column } from 'components/Containers';
+import Loader from 'components/Loader';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useGetKnownsUsersQuery } from 'store/user.api';
 import { MembersModalBody, ModalBodyElementWrapper, ModalSeparator, ModalText } from './index.styled';
+import UserRow from './UserRow';
 
 const AddMemberModal = () => {
   const { t } = useTranslation('groups');
+
+  const { data: knownsUsers, isLoading: isKnownsUsersLoading } = useGetKnownsUsersQuery();
   
   return (
     <Column>
@@ -17,6 +22,13 @@ const AddMemberModal = () => {
 
         <ModalBodyElementWrapper>
           <ModalText>{t('orLookAlreadyKnown')}</ModalText>
+
+          <Column fullWidth gap={'0.5rem'}>
+            {knownsUsers?.map(u => <UserRow key={u.id} user={u} onClick={() => {}} />)}
+          </Column>
+          
+          <Loader isLoading={isKnownsUsersLoading} />
+          {!!knownsUsers && knownsUsers.length === 0 && <ModalText>{t('noAlreadyKnownUsers')}</ModalText>}
         </ModalBodyElementWrapper>
       </MembersModalBody>
     </Column>
