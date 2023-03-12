e2c5fc8b8819c87661fb8fcc0afa3b09f23125d6
Vasyl Horbatenko <horbatenko.vasyl@gmail.com>
2023-03-12 12:44:27 +0200

    Add header to Account settings page

--- src/features/userProfile/accountSettings/index.tsx
+++ src/features/userProfile/accountSettings/index.tsx
@@ -1,8 +1,26 @@
+import { useCallback } from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+
 import { PageWrapper } from "components/Containers";
+import Header from "components/Header";
+import { ROUTES } from "constants/routes";
+import { ReactComponent as BackIconSVG } from 'assets/images/back-icon.svg';
 
 const AccountSetttings = () => {
+  const { t } = useTranslation('common');
+  const navigate = useNavigate();
+
+  const handleBackAction = useCallback(() => {
+    navigate(ROUTES.GROUPS.ROOT, { replace: true });
+  }, [navigate]);
+
   return (
     <PageWrapper>
+      <Header
+        text={t('accountSettings')}
+        leftActionComponent={<BackIconSVG onClick={handleBackAction} />}
+      />
     </PageWrapper>
   );
 };
