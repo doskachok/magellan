827f1845ec526670bd02ce5d9e1ae8369ed452d0
Alevtyna Yarychevska <ayarychevska@gmail.com>
2023-03-19 13:49:39 +0100

    error middleware displays errors based on api response (sometimes errors come not from details)

--- src/store/errorMiddleware.ts
+++ src/store/errorMiddleware.ts
@@ -1,17 +1,36 @@
-import {Middleware} from 'redux';
-import {addError} from './errorSlice';
-import {RootState} from './index';
+import { Middleware } from 'redux';
+import { addError } from './errorSlice';
+import { RootState } from './index';
 
 export const errorMiddleware: Middleware<{}, RootState> = store => next => action => {
   if (action?.error && !action.meta.condition) {
-    const message = action.payload?.data?.detail ? action.payload.data.detail :
-      action.payload?.status === 'FETCH_ERROR' ? 'Network error, please try again' : null;
 
-    if (message) {
+    let messages: Array<string> = Array<string>(0);
+
+    if (action.payload?.data?.detail) {
+      messages.push(action.payload?.data?.detail);
+    }
+
+    if (action.payload?.data?.errors) {
+      let combinedErrors: Array<string> = Array<string>(0);
+      Object.keys(action.payload?.data?.errors).forEach(field => {
+        combinedErrors.push(...(action.payload?.data?.errors[field] as Array<string>).map(errorMsg => {
+          return `${errorMsg}\n`;
+        }));
+      })
+      console.log(JSON.stringify(combinedErrors));
+      messages.push(...combinedErrors);
+    }
+
+    if (action.payload?.status === 'FETCH_ERROR') {
+      messages.push('Network error, please try again');
+    }
+
+    messages.forEach(message => {
       store.dispatch(addError({
         message,
       }));
-    }
+    })
   }
   return next(action);
 }
