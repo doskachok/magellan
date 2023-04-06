c16959f3e06a3bf3af2a23b3982acc6d3d5b4127
Vasyl Horbatenko <horbatenko.vasyl@gmail.com>
2023-04-06 18:25:03 +0300

    Add types needed to create transaction.

---
+++ src/features/expenses/types.ts
@@ -0,0 +1,19 @@
+export interface ICreateTransaction {
+  name: string;
+  paymentDateUtc: Date;
+  currencyCode: string;
+  groupId: string;
+  payerDetails: IPayerDetails[];
+  partialsAssignments: IPartialAssignments[];
+}
+
+export interface IPayerDetails {
+  payerId: string;
+  amount: number;
+}
+
+export interface IPartialAssignments {
+  userId: string;
+  partialAmount: number;
+  splitType: number; // TODO: introduce enum
+}
