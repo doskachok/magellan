f20c3ea0d5e2d6c41206075f6954b9e2d7d74831
Vasyl Horbatenko <horbatenko.vasyl@gmail.com>
2023-04-22 11:50:49 +0300

    Implement AddPayers page with common elements.

---
+++ src/features/expenses/AddExpense/AddPayers.styled.ts
@@ -0,0 +1,50 @@
+import { TextUnderline } from "components";
+import { ButtonTransparent } from "components";
+import { Column, Row } from "components/Containers";
+import styled from "styled-components";
+
+export const ContentWrapper = styled(Column)`
+  overflow-y: auto;
+  overflow-x: hidden;
+  justify-content: space-between;
+  height: 100%;
+  flex: 1;
+`;
+
+export const AddPayersWrapper = styled(Column)`
+  background: ${props => props.theme.colors.primary};
+  z-index: 1;
+  padding: 14px 1.5rem 0;
+  flex: 1;
+`;
+
+export const BackgroundFiller = styled(Row)`
+  background: ${props => props.theme.colors.primary};
+  width: 100%;
+  flex: 1;
+`;
+
+export const HalfCircleBackground = styled(Row)`
+  border-radius: 100%;
+  background: ${props => props.theme.colors.primary};
+  width: 120vw;
+  height: 80vw;
+  z-index: 0;
+  margin-top: -60vw;
+  margin-left: -10vw;
+`;
+
+export const NextStepButton = styled(ButtonTransparent)`
+  display: inline-flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  margin-top: 1.5rem;
+  margin-bottom: 3.5rem;
+
+  &:disabled ${TextUnderline} {
+    color: ${props => props.theme.colors.input.disabled.default};
+  }
+`;
+
---
+++ src/features/expenses/AddExpense/AddPayers.tsx
@@ -0,0 +1,77 @@
+import { useCallback, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { TextUnderline } from "components";
+import { PageWrapper, Row } from "components/Containers";
+import Header from "components/Header";
+import { ReactComponent as BackIconSVG } from 'assets/images/back-icon.svg';
+import { ReactComponent as ArrowRightSVG } from 'assets/images/arrow-right.svg';
+import { ReactComponent as ArrowRightDisabledSVG } from 'assets/images/arrow-right-disabled.svg';
+import { BackgroundFiller, ContentWrapper, HalfCircleBackground, AddPayersWrapper, NextStepButton } from "./AddPayers.styled";
+import BottomNavigation from "components/BottomNavigation";
+import currencies from "constants/currencies";
+import { ICreateTransaction } from "../types";
+import { newTransactionSelector, saveTransaction } from "../slice";
+import { CreateRouteString, ExpenseRouteMode, composeExpenseRoute } from "constants/routes";
+
+const AddPayers = () => {
+  const { t } = useTranslation('expenses');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const transaction = useSelector(newTransactionSelector);
+
+  const [form, /*setForm*/] = useState<ICreateTransaction>({
+    name: '',
+    paymentDateUtc: new Date().toISOString().split('T')[0],
+    currencyCode: currencies[0].value,
+    groupId: '',
+    payerDetails: [],
+    partialsAssignments: [],
+    ...(transaction ? transaction : {}), // Load transaction from the slice
+  });
+
+  const isNextStepButtonDisabled = true;
+
+  const handleBackAction = useCallback(() => {
+    navigate(-1);
+  }, [navigate]);
+
+  const onNextStep = () => {
+    dispatch(saveTransaction(form));
+    // TODO: navigate to the next step
+    navigate(composeExpenseRoute(form.groupId, CreateRouteString, ExpenseRouteMode.ADD_MAININFO));
+  }
+
+  return (
+    <PageWrapper>
+      <Header
+        text={t('payers')}
+        leftActionComponent={<BackIconSVG onClick={handleBackAction} />}
+      />
+
+      <ContentWrapper fullWidth>
+        <AddPayersWrapper fullWidth>
+          {/* TODO: Implement add payers */}
+          <BackgroundFiller />
+        </AddPayersWrapper>
+        <HalfCircleBackground />
+
+        <Row fullWidth jc={'center'}>
+          <NextStepButton disabled={isNextStepButtonDisabled} onClick={onNextStep}>
+            <TextUnderline>
+              {t('nextStep')}
+            </TextUnderline>
+            {!isNextStepButtonDisabled ? <ArrowRightSVG /> : <ArrowRightDisabledSVG />}
+          </NextStepButton>
+        </Row>
+      </ContentWrapper>
+
+      <BottomNavigation />
+    </PageWrapper>
+  );
+};
+
+export default AddPayers;
