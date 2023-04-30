2a1bf0ac3d1994cc70cc6f044c028a8662f270f3
Vasyl Horbatenko <horbatenko.vasyl@gmail.com>
2023-04-30 12:54:35 +0300

    Implement EquallySplitMethodView.

--- src/features/expenses/AddExpense/SplitMethods/Equally.tsx
+++ src/features/expenses/AddExpense/SplitMethods/Equally.tsx
@@ -1,17 +1,28 @@
+import { Column } from "components/Containers";
+import UserListItem from "components/UserListItem";
 import { ICreateTransaction } from "features/expenses/types";
 import { ITransactionGroup } from "features/groups/types";
+import { getCurrencyWithSymbolString } from "helpers/currencyHelper";
+
+import { CurrencyText } from "./Equally.styled";
 
 interface IProps {
   transaction: ICreateTransaction;
   group: ITransactionGroup;
 }
 
-// TODO
 const EquallySplitMethodView = ({ transaction, group }: IProps) => {
+  const totalAmount = transaction.payerDetails.reduce((acc, curr) => acc + curr.amount, 0);
+  const amountPerUser = totalAmount / transaction.partialsAssignments.length;
+
   return (
-    <div>
-      <h3>EquallySplitMethodView</h3>
-    </div>
+    <Column fullWidth gap={'0.5rem'}>
+      {transaction.partialsAssignments.map(u => <UserListItem
+        key={u.userId}
+        user={group.participants.find(p => p.id === u.userId)!}
+        rightItem={<CurrencyText>{getCurrencyWithSymbolString(amountPerUser, transaction.currencyCode)}</CurrencyText>}
+      />)}
+    </Column>
   );
 };
 
