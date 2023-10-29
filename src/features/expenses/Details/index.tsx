135ed8be040e8c577e10d95cd01c132ce973a824
Vasyl Horbatenko <horbatenko.vasyl@gmail.com>
2023-10-29 16:42:11 +0200

    Implement simple expense details view.

--- src/features/expenses/Details/index.tsx
+++ src/features/expenses/Details/index.tsx
@@ -9,6 +9,9 @@ import Loader from 'components/Loader';
 import { ReactComponent as BackIconSVG } from 'assets/images/back-icon.svg';
 import { useLazyGetTransactionByIdQuery } from '../api';
 import { selectTransactionById } from '../slice';
+import PaymentRow from './PaymentRow';
+import { selectedGroupSelector } from 'features/groups/slice';
+import { PaymentListContainer, PaymentListTitleText } from './index.styled';
 
 
 const ExpenseDetails = () => {
@@ -17,6 +20,7 @@ const ExpenseDetails = () => {
 
   const [getExpense, { isLoading }] = useLazyGetTransactionByIdQuery();
   const expense = useSelector(selectTransactionById(expenseId || ''));
+  const group = useSelector(selectedGroupSelector);
 
   const handleBackAction = useCallback(() => {
     navigate(-1);
@@ -37,6 +41,33 @@ const ExpenseDetails = () => {
       <ContentWrapper fullWidth>
         <HalfCircle />
 
+        <PaymentListTitleText> {"Payers:"} </PaymentListTitleText>
+        <PaymentListContainer>
+          {expense?.payerDetails.map((pd) => {
+            const user = group?.participants.find((p) => p.id === pd.payerId);
+            return (
+              <PaymentRow key={pd.payerId}
+                // TODO: query user by id when user is not in the group instead of showing payerId
+                user={user?.name || user?.email || pd.payerId} 
+                amount={pd.amount}
+                currencyCode={expense.currencyCode} />
+            );
+          })}
+        </PaymentListContainer>
+
+        <PaymentListTitleText> {"Participants:"} </PaymentListTitleText>
+        <PaymentListContainer>
+          {expense?.partialsAssignments.map((pa) => {
+            const user = group?.participants.find((p) => p.id === pa.userId);
+            return (
+              <PaymentRow key={pa.userId}
+                // TODO: query user by id when user is not in the group instead of showing userId
+                user={user?.name || user?.email || pa.userId} 
+                amount={pa.partialAmount}
+                currencyCode={expense.currencyCode} />
+            );
+          })}
+        </PaymentListContainer>
       </ContentWrapper>
       <Loader isLoading={isLoading}/>
     </PageWrapper>
