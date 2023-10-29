ab56c79926b3c32a81686e129adf3c1389ca8da2
Vasyl Horbatenko <horbatenko.vasyl@gmail.com>
2023-10-29 12:46:28 +0200

    Add redirection to the expense details page when clicking on expense row in a group details page.

--- src/features/groups/Details/TransactionRow/index.tsx
+++ src/features/groups/Details/TransactionRow/index.tsx
@@ -1,7 +1,11 @@
+import { useParams } from 'react-router';
+import { useTranslation } from 'react-i18next';
+
 import { Column } from 'components/Containers';
+import { RowWrapperLink } from 'components/Link';
+import { ExpenseRouteMode, composeExpenseRoute } from 'constants/routes';
 import { ITransaction } from 'features/groups/types';
 import { getCurrencyWithSymbolString } from 'helpers/currencyHelper';
-import { useTranslation } from 'react-i18next';
 import { TransactionAmountText, TransactionNameText, TransactionRowContainer, YourPartAmount, YourPartAmountHint } from './index.styled';
 
 export interface ITransactionRowProps {
@@ -10,6 +14,7 @@ export interface ITransactionRowProps {
 
 const TransactionRow = ({ transaction }: ITransactionRowProps) => {
   const { t } = useTranslation('groups');
+  const { groupId } = useParams();
 
   const transactionAttendanceHint = (amount: number): string => {
     if (amount > 0) return t('transactionYouLent'); 
@@ -18,22 +23,24 @@ const TransactionRow = ({ transaction }: ITransactionRowProps) => {
   }
 
   return (
-    <TransactionRowContainer>
-      <Column>
-        <TransactionNameText> {transaction.name} </TransactionNameText>
-        <TransactionAmountText>
-          {getCurrencyWithSymbolString(transaction.totalAmount, transaction.currencyCode)}
-        </TransactionAmountText>
-      </Column>
-      <Column>
-        <YourPartAmountHint amount={transaction.yourPart}>
-          {transactionAttendanceHint(transaction.yourPart)}
-        </YourPartAmountHint>
-        <YourPartAmount amount={transaction.yourPart}>
-          {getCurrencyWithSymbolString(Math.abs(transaction.yourPart), transaction.currencyCode)}
-        </YourPartAmount>
-      </Column>
-    </TransactionRowContainer>
+    <RowWrapperLink to={composeExpenseRoute(groupId ?? "", transaction.id, ExpenseRouteMode.NONE)}>
+      <TransactionRowContainer>
+        <Column>
+          <TransactionNameText> {transaction.name} </TransactionNameText>
+          <TransactionAmountText>
+            {getCurrencyWithSymbolString(transaction.totalAmount, transaction.currencyCode)}
+          </TransactionAmountText>
+        </Column>
+        <Column>
+          <YourPartAmountHint amount={transaction.yourPart}>
+            {transactionAttendanceHint(transaction.yourPart)}
+          </YourPartAmountHint>
+          <YourPartAmount amount={transaction.yourPart}>
+            {getCurrencyWithSymbolString(Math.abs(transaction.yourPart), transaction.currencyCode)}
+          </YourPartAmount>
+        </Column>
+      </TransactionRowContainer>
+    </RowWrapperLink>
   );
 };
 
