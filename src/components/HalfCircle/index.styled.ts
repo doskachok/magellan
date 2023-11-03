087fe1171508e41e42c77daaa7d4e30d29deb34a
Denys Krutko <krutko.den@gmail.com>
2023-11-03 23:47:05 +0100

    Half circle styles no longer cross component borders

--- src/components/HalfCircle/index.styled.ts
+++ src/components/HalfCircle/index.styled.ts
@@ -2,10 +2,11 @@ import { Row } from 'components/Containers';
 import styled from 'styled-components';
 
 export const HalfCircle = styled(Row)`
-  border-radius: 100%;
+  border-radius: 50% / 100%;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
   background: ${props => props.theme.colors.primary};
-  width: 120vw;
-  min-height: 300px;
+  width: 100vw;
+  min-height: 60px;
   z-index: 0;
-  margin: -230px 0 0 -10vw;
 `;
